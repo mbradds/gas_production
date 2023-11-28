9f3f61b9f4140db463769b15a07a2d6c59dd3b41
Grant Moss <grantmossdev@gmail.com>
2023-11-28 10:38:22 -0700

    started on main view

--- aer.sql
+++ aer.sql
@@ -1,5 +1,9 @@
+--create view vwMarketableGasProduction as
+
+with ab_cte as
+(
 select [Date], 
-([Well Production] + [In Situ Well Production]) - ([Flare]+[Fuel]+[Vent]+[Shrinkage]+[Acid Gas Injection]+[Gas Injection]) as [Marketable Gas (103m3)]
+([Well Production] + [In Situ Well Production]) - ([Flare]+[Fuel]+[Vent]+[Shrinkage]+[Acid Gas Injection]+[Gas Injection]) as [AB Marketable Gas (103m3)]
 from
 (
 
@@ -33,3 +37,23 @@ PIVOT
 (
 MAX([Value]) FOR Category IN ([Well Production], [In Situ Well Production], [Flare], [Fuel], [Vent], [Shrinkage], [Acid Gas Injection], [Gas Injection])
 ) as PivotTable
+),
+sk_cte as
+(
+SELECT
+[Production Month],
+sum([Total Gas Production (10^3m3)]) as [Total Gas Production (10^3m3)],
+sum([Gas Available for Use or Sale (10^3m3)]) as [SK Marketable Gas (103m3)]
+FROM [EnergyData].[dbo].[SK_Production_Gas]
+group by [Production Month]
+)
+
+
+select 
+[Date],
+DATEDIFF(DAY, DATEFROMPARTS(YEAR([Date]), MONTH([Date]), 1), EOMONTH([Date])) + 1 AS DaysInMonth,
+[AB Marketable Gas (103m3)],
+[SK Marketable Gas (103m3)]
+from ab_cte full outer join sk_cte on ab_cte.Date = sk_cte.[Production Month]
+
+order by [Date] desc
